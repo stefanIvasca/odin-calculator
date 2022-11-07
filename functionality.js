2bc1622787d49a7a63b55a632c48e3e296177745
stefanIvasca <stefanivasca@gmail.com>
2022-11-07 11:30:34 +0200

    add functionality for operation with two numbers

--- functionality.js
+++ functionality.js
@@ -10,4 +10,48 @@ function operate(operator,firstNum,secondNum){
     }
 }
 
-console.log(operate('+',7,9));
+let id=document.querySelectorAll('button');
+let screenValue=document.querySelector('.eventDisplay');
+let result=document.querySelector('.result');
+
+let firstNum='';
+let secondNum='';
+let operator='';
+
+
+function displayValue(indexNum){
+    if (operator===''){
+    firstNum+=id[indexNum].innerText;
+    screenValue.innerText=firstNum;
+    }else{
+    secondNum+=id[indexNum].innerText;
+    screenValue.innerText=secondNum;
+    }
+}
+
+function displayOperator(indexNum){
+    screenValue.innerText=id[indexNum].innerText;
+    operator=id[indexNum].innerText;
+    console.log(operator);
+}
+
+function clearDisplay(){
+    screenValue.innerText='';
+    firstNum='';
+    secondNum='';
+    operator='';
+    result.innerText='';
+
+}
+
+function returnResult(){
+    if(operator==='+'){
+        result.innerText= +firstNum+ +secondNum;
+    }else if(operator==='−'){
+        result.innerText= +firstNum- +secondNum;
+    }else if(operator==='×'){
+        result.innerText= +firstNum* +secondNum;
+    }else if(operator==='÷'){
+        result.innerText= +firstNum/ +secondNum;
+    }
+}
